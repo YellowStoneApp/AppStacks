5bbbbf0ea3b6322c8c3141413ee1368bde267179
Ryan Barnes <rpbarnes9@gmail.com>
2021-04-19 07:04:39 -0700

    Initial commit

---
+++ src/AppStacks/Program.cs
@@ -0,0 +1,15 @@
+﻿using Amazon.CDK;
+
+namespace AppStacks
+{
+    sealed class Program
+    {
+        public static void Main(string[] args)
+        {
+            var app = new App();
+            new AppStacksStack(app, "AppStacksStack");
+
+            app.Synth();
+        }
+    }
+}
